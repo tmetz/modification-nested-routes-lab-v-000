8d6a810fa35e26b12b53d25797b76bd2af9a8a3b
Tammy Metz <tammy@tammymetz.com>
2019-03-09 12:57:45 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,5 +1,5 @@
 module SongsHelper
-  def artist_id_field(song)
+  def artist_select(song)
     if song.artist.nil?
       select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     else
